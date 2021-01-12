9e5eb2479266228c5ce7ed8e79108806df7ddba8
nkohler96 <77270943+nkohler96@users.noreply.github.com>
2021-01-12 09:56:02 +0100

    Add files via upload

---
+++ Kategorie.java
@@ -0,0 +1,19 @@
+
+public class Kategorie {
+
+	private String Kategorie;
+	
+	
+	public Kategorie(String Kategorie) {
+		this.Kategorie = Kategorie;
+	}
+	
+	public void setKategorie(String neueKategorie) {
+		this.Kategorie = neueKategorie
+	}
+	
+	public String getKategorie() {
+		return this.Kategorie;
+	}
+
+}
