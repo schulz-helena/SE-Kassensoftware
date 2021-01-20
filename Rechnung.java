e2197261fca2aff35c2181e08ea31851e1dccfd0
Andreas Niemann <andreasniemann1999@icloud.com>
2021-01-20 15:53:17 +0100

    Create Rechnung.java

---
+++ Rechnung.java
@@ -0,0 +1,26 @@
+
+public class Rechnung {
+	private float zwischensumme;
+	private float endsumme;
+	
+	// private Produktliste liste;
+	
+	// Konstruktor
+	public Rechnung() {
+		
+	}
+	
+	public float getEndsumme() {
+		return this.endsumme;
+	}
+	
+	public void setEndsumme(float neueEndsumme) {
+		this.endsumme = neueEndsumme;
+	}
+	
+	public void setProdukte() {
+		
+	}
+	
+	
+}
