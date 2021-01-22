6bb1cfc78c8819f9e331442f189de53a532d4eb0
nkohler96 <77270943+nkohler96@users.noreply.github.com>
2021-01-22 11:09:07 +0100

    Add files via upload

---
+++ Kassensoftware/src/kassensoftware/Artikel.java
@@ -0,0 +1,18 @@
+
+public class Artikel {
+
+	private String Artikelname;
+	
+	public Artikel(String Artikelname) {
+		this.setArtikelname(Artikelname);
+	}
+	
+	public String getArtikelname() {
+		return this.Artikelname;
+	}
+
+	public void setArtikelname(String artikelname) {
+		this.Artikelname = artikelname;
+	}
+
+}
---
+++ Kassensoftware/src/kassensoftware/Einkauf.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Nils
+ *
+ */
+
+public class Einkauf {
+	
+	
+	public static void produktEinkaufen(Artikel a) {
+		Einkaufsliste.neuerArtikel(a);
+		
+	}
+	
+	
+}
