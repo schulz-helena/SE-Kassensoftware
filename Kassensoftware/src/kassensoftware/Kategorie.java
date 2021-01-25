a41960d0b547581dec519c9a32fb6fa387362944
nkohler96 <77270943+nkohler96@users.noreply.github.com>
2021-01-25 11:16:59 +0100

    Add files via upload

---
+++ Kassensoftware/src/kassensoftware/Kategorie.java
@@ -0,0 +1,19 @@
+
+public class Kategorie {
+
+	private String kategorieName;
+
+	public Kategorie(String kategorieNeu) {
+		this.setKategorieName(kategorieNeu);
+	}
+
+	public String getKategorieName() {
+		return kategorieName;
+	}
+
+	public void setKategorieName(String kategorieName) {
+		this.kategorieName = kategorieName;
+	}
+
+	
+}
