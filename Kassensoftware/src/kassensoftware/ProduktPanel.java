ed15e09ae403aaaf39b74a102c049356450db741
Helena Schulz <76915164+schulz-helena@users.noreply.github.com>
2021-01-31 20:31:26 +0100

    Update ProduktPanel.java

--- Kassensoftware/src/kassensoftware/ProduktPanel.java
+++ Kassensoftware/src/kassensoftware/ProduktPanel.java
@@ -52,7 +52,7 @@ public class ProduktPanel extends JPanel {
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 110, 0, 40, 0, -1, 0, 22, 0, 0, 0, 0, 0, 0, 0, 49, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{90, 65, 65, 65, 65, 65, 80, 92, 0};
+		gridBagLayout.rowHeights = new int[]{57, 65, 65, 65, 65, 65, 80, 84, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
